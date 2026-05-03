54aaf9ac1ddbe94b78ee9b8bb55e2c8dc91cdd7d
riliu <ilyukroma228@gmail.com>
2026-05-03 20:38:06 +0300

    feat: create a new guest on first sign in

--- app/account/page.js
+++ app/account/page.js
@@ -6,11 +6,11 @@ export const metadata = {
 
 export default async function Page() {
   const session = await auth();
-  const firstName = session.user.name.split("").at(0);
+  const firstName = session.user.name.split(" ").at(0);
 
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-      Welcome, {session.user.name}
+      Welcome, {firstName}
     </h2>
   );
 }
